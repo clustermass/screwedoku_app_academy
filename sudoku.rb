7a90159b2b12a78ba3ecbef8b62ff23922075ff8
Shawn Salat <Shawn.Salat@gmail.com>
2018-04-19 14:58:14 -0700

    Switch get_pos and get_val logic

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,4 @@
 require_relative "board"
-
 class SudokuGame
   def self.from_file(filename)
     board = Board.from_file(filename)
@@ -10,7 +9,7 @@ class SudokuGame
     @board = board
   end
 
-  def get_pos
+  def get_val
     pos = nil
     until pos && valid_val?(pos)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
@@ -20,7 +19,7 @@ class SudokuGame
     pos
   end
 
-  def get_val
+  def get_pos
     val = nil
     until val && valid_pos?(val)
       puts "Please enter a position on the board (e.g., '3,4')"
@@ -49,8 +48,8 @@ class SudokuGame
   def play_turn
     board.render
 
-    val = get_val
     pos = get_pos
+    val = get_val
 
     board[pos] = val
   end
